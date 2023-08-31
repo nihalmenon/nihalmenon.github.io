9e1203c70fb0a6c8ebfb9c8ec145f4fd70c1c0d0
Nihal Menon <=>
2023-08-31 16:00:57 -0500

    update

--- src/components/ExperienceShow.js
+++ src/components/ExperienceShow.js
@@ -4,7 +4,7 @@ function ExperienceShow() {
     const companies = ['BTNX']
     const positions = ['Full-Stack Developer']
     const durations = ['May 2023 - Aug 2023']
-    const descriptions = ['I worked on the internal manager site, customer-facing website, and the warehouse app for a biotechnology company with a worldwide presence. I’ve led transformative projects, such as enhancing tax calculations by creating web scrapers, designing a new purchase order system, automating data retrieval for inventory reports, designing enhanced login systems for customers, and a range of other impactful initiatives.']
+    const descriptions = ['I worked on the internal manager site, customer-facing website, and the warehouse app for a biotechnology company with a worldwide presence. I’ve led transformative projects, such as enhancing tax calculations by creating web scrapers, designing a new purchase order system, automating data retrieval for inventory reports, developing enhanced login systems for customers, and a range of other impactful initiatives.']
     const tools = [['React.js, C#, SQL, Angular, LINQ, Python, Selenium, Razor, Blazor']]
 
     const experiences = companies.map((company, i) => {
