2eed8690ffce59d13c5ce2d26e07c8ff7410c9fb
Nihal Menon <=>
2023-09-15 01:53:23 -0400

    changed position title

--- src/components/ExperienceShow.js
+++ src/components/ExperienceShow.js
@@ -2,7 +2,7 @@ import ExperienceCard from "./ExperienceCard"
 
 function ExperienceShow() {
     const companies = ['BTNX']
-    const positions = ['Full-Stack Developer']
+    const positions = ['Software Developer']
     const durations = ['May 2023 - Aug 2023']
     const descriptions = ['I worked on the internal manager site, customer-facing website, and the warehouse app for a biotechnology company with a worldwide presence. I’ve led transformative projects, such as enhancing tax calculations by creating web scrapers, designing a new purchase order system, automating data retrieval for inventory reports, developing enhanced login systems for customers, and a range of other impactful initiatives.']
     const tools = [['React.js, C#, SQL, Angular, LINQ, Python, Selenium, Razor, Blazor']]
