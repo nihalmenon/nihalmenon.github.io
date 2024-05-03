4a333116504099525489fe39db3b318db0a91eda
Nihal Menon <=>
2024-05-03 13:54:13 -0400

    updates

--- src/components/ExperienceShow.js
+++ src/components/ExperienceShow.js
@@ -1,13 +1,14 @@
 import ExperienceCard from "./ExperienceCard"
 
 function ExperienceShow() {
-    const companies = ['BTNX']
-    const positions = ['Software Developer']
-    const durations = ['May 2023 - Aug 2023']
-    const descriptions = ['I worked on the internal manager site, customer-facing website, and the warehouse app for a biotechnology company with a worldwide presence. I’ve led transformative projects, such as enhancing tax calculations by creating web scrapers, designing a new purchase order system, automating data retrieval for inventory reports, developing enhanced login systems for customers, and a range of other impactful initiatives.']
-    const tools = [['React.js, C#, SQL, Angular, LINQ, Python, Selenium, Razor, Blazor']]
+    const companies = ['BTNX', 'Sun Life']
+    const positions = ['Software Developer', 'Software Engineer Intern']
+    const durations = ['May 2023 - Aug 2023', 'Jan 2024 - Apr 2024']
+    const descriptions = ['I led projects for a global biotechnology company such as enhancing tax calculations by creating web scrapers, designing a new purchase order system, automating data retrieval for inventory reports, developing enhanced login systems for customers, and a range of other impactful initiatives.', 'I worked on the Secure Web Platform team, developing a new Redux Toolkit feature as well as creating a robust GraphQL API.']
+    const tools = [['React.js, C#, SQL, Angular, LINQ, Python, Selenium, Razor, Blazor'], ['GraphQL', 'Java Spring Boot', 'Node.js', 'React.js', 'Redux']]
 
-    const experiences = companies.map((company, i) => {
+    const experiences = companies.reverse().map((company, index) => {
+        const i = companies.length - index - 1;
         return <ExperienceCard company={company} position={positions[i]} description={descriptions[i]} duration={durations[i]} tools={tools[i]} key={i}/>
     })
 
--- src/components/ProjectShow.js
+++ src/components/ProjectShow.js
@@ -8,22 +8,24 @@ import never4get from '../img/never4get.png'
 import Spel from '../img/spel.png';
 import Coronavirus from '../img/coronavirus.png';
 import Snake from '../img/snake.png';
+import flashify from '../img/flashify.png';
 
 function ProjectShow() {
-    const projects = ['Spel', 'ForAllTimes', 'BeTreel', 'Python Breast Cancer Malignance Classifier', 'Python Dementia Expression Classifier', 'Trivia Game', 'never4get', 'covidata', 'Snake']
-    const descriptions = ['Spel is a single and multiplayer spelling game to improve spelling/vocabulary skills under time pressure. Choose to play solo or connect with friends in multiplayer mode, all backed by a secure server and live interactions. It was built on the MERN stack and uses Socket.io to implement real time interactions. ', 'ForAllTimes is a lounge traffic monitor made for software engineering students at the University of Waterloo. It uses a Raspberry Pi and Ultrasonic sensors to detect when people enter and exit a room. It then updates a counter using an AWS Lambda function. The backend then uses Node.js and Express.js to display this on a website.', 'BeTreel is a proof of concept for a social media app that prompts users to post themselves engaging in environmentally-friendly activities daily. Mimics BeReal\'s concept of one universal notification daily to cue a user to create a post.', 'Python machine learning model using Scikit-learn and pandas that classified tumors as malignant or benign with 90% accuracy based on factors such as cell size, cell uniformity etc. using an online public dataset.', 'Python machine learning model using Scikit-learn and Pandas that predicted whether patients expressed dementia or not based on factors such as mental test scores, intracranial volume, age etc. using a public dataset.', 'This is a simple trivia game using the open trivia API. It was built with jQuery, HTML, and CSS.', 'Never4get is a website that helps you organize all the things you have to do. With an easy to use interface, you can reorder, rename, delete, and check items off as you complete all of your tasks.', 'A resource to display the number of cases, deaths, and recoveries by date in each county/state using NY Times data.', 'A remake of the classic retro snake game. Maneuver around the map and navigate to food in order to get longer and increase your score.']
-    const links = ['https://spelapp.netlify.app/', 'https://foralltimes.cyclic.app', 'https://devpost.com/software/betreel', 'https://github.com/nihalmenon/BreastCancerMalignanceClassifier', 'https://github.com/nihalmenon/Dementia', 'https://nihalmenon.com/trivia', 'https://nihalmenon.com/never4get', 'https://nihalmenon.com/coronavirus/', 'https://nihalmenon.com/snake/']
+    const projects = ['Spel', 'Flashify', 'ForAllTimes', 'BeTreel', 'Python Breast Cancer Malignance Classifier', 'Python Dementia Expression Classifier', 'Trivia Game', 'never4get', 'covidata', 'Snake']
+    const descriptions = ['Spel is a single and multiplayer spelling game to improve spelling/vocabulary skills under time pressure. Choose to play solo or connect with friends in multiplayer mode, all backed by a secure server and live interactions. It was built on the MERN stack and uses Socket.io to implement real time interactions. ', 'Flashify is a web app designed to streamline the process of converting handwritten notes into interactive flashcards. Users can upload an image and our AI-powered system will convert the text into question-answer style digital flashcards. This project uses Flask, React, MySQL, and the OpenAI API to manage the flashcard system.', 'ForAllTimes is a lounge traffic monitor made for software engineering students at the University of Waterloo. It uses a Raspberry Pi and Ultrasonic sensors to detect when people enter and exit a room. It then updates a counter using an AWS Lambda function. The backend then uses Node.js and Express.js to display this on a website.', 'BeTreel is a proof of concept for a social media app that prompts users to post themselves engaging in environmentally-friendly activities daily. Mimics BeReal\'s concept of one universal notification daily to cue a user to create a post.', 'Python machine learning model using Scikit-learn and pandas that classified tumors as malignant or benign with 90% accuracy based on factors such as cell size, cell uniformity etc. using an online public dataset.', 'Python machine learning model using Scikit-learn and Pandas that predicted whether patients expressed dementia or not based on factors such as mental test scores, intracranial volume, age etc. using a public dataset.', 'This is a simple trivia game using the open trivia API. It was built with jQuery, HTML, and CSS.', 'Never4get is a website that helps you organize all the things you have to do. With an easy to use interface, you can reorder, rename, delete, and check items off as you complete all of your tasks.', 'A resource to display the number of cases, deaths, and recoveries by date in each county/state using NY Times data.', 'A remake of the classic retro snake game. Maneuver around the map and navigate to food in order to get longer and increase your score.']
+    const links = ['https://spelapp.netlify.app/', 'https://devpost.com/software/flashify-o48guw', 'https://foralltimes.cyclic.app', 'https://devpost.com/software/betreel', 'https://github.com/nihalmenon/BreastCancerMalignanceClassifier', 'https://github.com/nihalmenon/Dementia', 'https://nihalmenon.com/trivia', 'https://nihalmenon.com/never4get', 'https://nihalmenon.com/coronavirus/', 'https://nihalmenon.com/snake/']
 
     const imgMap = {
         0: Spel,
-        1: foralltimes,
-        2: BeTreel,
-        3: Github,
+        1: flashify,
+        2: foralltimes,
+        3: BeTreel,
         4: Github,
-        5: trivia,
-        6: never4get,
-        7: Coronavirus,
-        8: Snake
+        5: Github,
+        6: trivia,
+        7: never4get,
+        8: Coronavirus,
+        9: Snake
     }
 
     const renderedProjects = projects.map((title, index) => {
